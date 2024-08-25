d0dc9996ef307a1e0065e3fa173ebe084e50e4ee
Queiroz1992 <51212831+Queiroz1992@users.noreply.github.com>
2024-08-25 12:42:03 -0300

    Criação do projeto e estrutura (components, models, services)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LivroFormComponent } from './components/livro-form/livro-form.component';
+import { ExcluirComponent } from './components/excluir/excluir.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivroFormComponent,
+    ExcluirComponent
   ],
   imports: [
     BrowserModule,
